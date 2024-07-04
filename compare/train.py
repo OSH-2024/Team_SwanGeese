3cb1edfa47cc5d3e4af4da60f2ec652dd9c9cb11
YueqiangHe <yueqiang_he@mail.ustc.edu.cn>
2024-07-04 09:57:58 +0800

    更新train.py

--- compare/train.py
+++ compare/train.py
@@ -1,145 +1,104 @@
-import pandas as pd
+import time
 import torch
+import pandas as pd
 from datasets import Dataset
 from transformers import AutoTokenizer, AutoModelForCausalLM, DataCollatorForSeq2Seq, TrainingArguments, Trainer
-from modelscope import snapshot_download
-from peft import LoraConfig, TaskType, get_peft_model, PeftModel
+from peft import LoraConfig, TaskType, get_peft_model
 
-# 需要微调的基座模型
-# https://www.modelscope.cn/studios/LLM-Research/Chat_Llama-3-8B/summary
-model_id = 'LLM-Research/Meta-Llama-3-8B-Instruct'
+# 将JSON文件转换为Dataset
+df = pd.read_json('./huanhuan.json')
+ds = Dataset.from_pandas(df)
 
-# https://www.modelscope.cn/models/qwen/Qwen1.5-4B-Chat/summary
-# model_id = 'qwen/Qwen1.5-4B-Chat'
+# 加载分词器
+tokenizer = AutoTokenizer.from_pretrained('/root/autodl-tmp/LLM-Research/Meta-Llama-3-8B-Instruct', use_fast=False, trust_remote_code=True)
+tokenizer.pad_token = tokenizer.eos_token
 
-# 检查CUDA是否可用，然后检查MPS是否可用，最后回退到CPU
-device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
+def process_func(example):
+    MAX_LENGTH = 384  # Llama分词器会将一个中文字切分为多个token，因此需要放开一些最大长度，保证数据的完整性
+    input_ids, attention_mask, labels = [], [], []
+    instruction = tokenizer(f"user\n\n{example['instruction'] + example['input']}assistant\n\n", add_special_tokens=False)
+    response = tokenizer(f"{example['output']}", add_special_tokens=False)
+    input_ids = instruction["input_ids"] + response["input_ids"] + [tokenizer.pad_token_id]
+    attention_mask = instruction["attention_mask"] + response["attention_mask"] + [1]  # 因为eos token也是要关注的所以补充为1
+    labels = [-100] * len(instruction["input_ids"]) + response["input_ids"] + [tokenizer.pad_token_id]
+    if len(input_ids) > MAX_LENGTH:  # 做一个截断
+        input_ids = input_ids[:MAX_LENGTH]
+        attention_mask = attention_mask[:MAX_LENGTH]
+        labels = labels[:MAX_LENGTH]
+    return {
+        "input_ids": input_ids,
+        "attention_mask": attention_mask,
+        "labels": labels
+    }
 
-models_dir = './models'
-dataset_file = './dataset/huanhuan.json'
-# modelscope/hub/snapshot_download.py:75 会把模型改名 name = name.replace('.', '___')
-model_path = f"{models_dir}/model/{model_id.replace('.', '___')}"
-checkpoint_dir = f"./models/checkpoint/{model_id}"
-lora_dir = f"./models/lora/{model_id}"
+tokenized_id = ds.map(process_func, remove_columns=ds.column_names)
 
-torch_dtype = torch.half
+tokenizer.decode(list(filter(lambda x: x != -100, tokenized_id[1]["labels"])))
 
-lora_config = LoraConfig(
+# 加载模型
+model = AutoModelForCausalLM.from_pretrained('/root/autodl-tmp/LLM-Research/Meta-Llama-3-8B-Instruct', device_map="auto", torch_dtype=torch.bfloat16)
+model.enable_input_require_grads()  # 开启梯度检查点时，要执行该方法
+
+# 配置LoRA
+config = LoraConfig(
     task_type=TaskType.CAUSAL_LM,
     target_modules=["q_proj", "k_proj", "v_proj", "o_proj", "gate_proj", "up_proj", "down_proj"],
-    inference_mode=False,
-    r=8,
-    lora_alpha=32,
-    lora_dropout=0.1
+    inference_mode=False,  # 训练模式
+    r=8,  # Lora 秩
+    lora_alpha=32,  # Lora alpha，具体作用参见 Lora 原理
+    lora_dropout=0.1  # Dropout 比例
+)
+model = get_peft_model(model, config)
+model.print_trainable_parameters()
+
+# 训练参数
+args = TrainingArguments(
+    output_dir="./output/llama3",
+    per_device_train_batch_size=4,
+    gradient_accumulation_steps=4,
+    logging_steps=10,
+    num_train_epochs=3,
+    save_steps=100,
+    learning_rate=1e-4,
+    save_on_each_node=True,
+    gradient_checkpointing=True
 )
 
+# 计算吞吐量
+class ThroughputTrainer(Trainer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.total_samples = 0
+        self.total_tokens = 0
+        self.start_time = None
 
-def train():
-    # 加载模型
-    model_dir = snapshot_download(model_id=model_id, cache_dir=f"{models_dir}/model", revision='master')
-    if model_path != model_dir:
-        raise Exception(f"model_path:{model_path} != model_dir:{model_dir}")
+    def on_train_begin(self, args, state, control, **kwargs):
+        self.start_time = time.time()
+    
+    def on_step_end(self, args, state, control, **kwargs):
+        self.total_samples += args.per_device_train_batch_size * args.gradient_accumulation_steps
+        self.total_tokens += sum(len(inputs['input_ids']) for inputs in kwargs['inputs'])
 
-    model = AutoModelForCausalLM.from_pretrained(model_path, device_map="auto", torch_dtype=torch_dtype)
-    model.enable_input_require_grads()  # 开启梯度检查点时，要执行该方法
+    def on_train_end(self, args, state, control, **kwargs):
+        end_time = time.time()
+        total_time = end_time - self.start_time
+        samples_per_second = self.total_samples / total_time
+        tokens_per_second = self.total_tokens / total_time
+        print(f"Training Throughput: {samples_per_second:.2f} samples/second")
+        print(f"Training Throughput: {tokens_per_second:.2f} tokens/second")
 
-    # 加载数据
-    df = pd.read_json(dataset_file)
-    ds = Dataset.from_pandas(df)
-    print(ds[:3])
+# 初始化Trainer
+trainer = ThroughputTrainer(
+    model=model,
+    args=args,
+    train_dataset=tokenized_id,
+    data_collator=DataCollatorForSeq2Seq(tokenizer=tokenizer, padding=True),
+)
 
-    # 处理数据
-    tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=False, trust_remote_code=True)
-    tokenizer.pad_token = tokenizer.eos_token
+# 开始训练
+trainer.train()
 
-    def process_func(item):
-        MAX_LENGTH = 384  # Llama分词器会将一个中文字切分为多个token，因此需要放开一些最大长度，保证数据的完整性
-        input_ids, attention_mask, labels = [], [], []
-        instruction = tokenizer(
-            f"<|start_header_id|>user<|end_header_id|>\n\n{item['instruction'] + item['input']}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n",
-            add_special_tokens=False)  # add_special_tokens 不在开头加 special_tokens
-        response = tokenizer(f"{item['output']}<|eot_id|>", add_special_tokens=False)
-        input_ids = instruction["input_ids"] + response["input_ids"] + [tokenizer.pad_token_id]
-        attention_mask = instruction["attention_mask"] + response["attention_mask"] + [1]  # 因为eos token咱们也是要关注的所以 补充为1
-        labels = [-100] * len(instruction["input_ids"]) + response["input_ids"] + [tokenizer.pad_token_id]
-        if len(input_ids) > MAX_LENGTH:  # 做一个截断
-            input_ids = input_ids[:MAX_LENGTH]
-            attention_mask = attention_mask[:MAX_LENGTH]
-            labels = labels[:MAX_LENGTH]
-        return {
-            "input_ids": input_ids,
-            "attention_mask": attention_mask,
-            "labels": labels
-        }
-
-    tokenized_id = ds.map(process_func, remove_columns=ds.column_names)
-
-    tokenizer.decode(list(filter(lambda x: x != -100, tokenized_id[1]["labels"])))
-
-    # 加载lora权重
-    model = get_peft_model(model, lora_config)
-
-    # 训练模型
-    training_args = TrainingArguments(
-        output_dir=checkpoint_dir,
-        per_device_train_batch_size=4,
-        gradient_accumulation_steps=4,
-        logging_steps=10,
-        num_train_epochs=3,
-        save_steps=100,
-        learning_rate=1e-4,
-        save_on_each_node=True,
-        gradient_checkpointing=True,
-    )
-
-    trainer = Trainer(
-        model=model,
-        args=training_args,
-        train_dataset=tokenized_id,
-        data_collator=DataCollatorForSeq2Seq(tokenizer=tokenizer, padding=True),
-    )
-    trainer.train()
-
-    # 保存模型
-    trainer.model.save_pretrained(lora_dir)
-    tokenizer.save_pretrained(lora_dir)
-
-
-def infer(prompt="你是谁？"):
-    print(f"prompt: {prompt}")
-    # 加载tokenizer
-    tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=False, trust_remote_code=True)
-
-    # 加载模型
-    model = AutoModelForCausalLM.from_pretrained(model_path, device_map="auto", torch_dtype=torch_dtype)
-
-    # 加载lora权重
-    model = PeftModel.from_pretrained(model, model_id=lora_dir, config=lora_config)
-
-    messages = [
-        {"role": "user", "content": prompt}
-    ]
-
-    text = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
-
-    model_inputs = tokenizer([text], return_tensors="pt").to(device)
-
-    generated_ids = model.generate(
-        model_inputs.input_ids,
-        max_new_tokens=512,
-        eos_token_id=tokenizer.encode('<|eot_id|>')[0],
-        pad_token_id=tokenizer.pad_token_id,
-        attention_mask=model_inputs.attention_mask,
-    )
-    generated_ids = [
-        output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
-    ]
-
-    response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
-    return response.strip()
-
-
-if __name__ == '__main__':
-    train()
-    res = infer(prompt="你是谁？")
-    print(res)
+# 保存模型和分词器
+peft_model_id = "./llama3_lora"
+trainer.model.save_pretrained(peft_model_id)
+tokenizer.save_pretrained(peft_model_id)
